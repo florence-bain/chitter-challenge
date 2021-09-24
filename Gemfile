b7b8b9e1aa59b7452f758ede69ccf2ce9d07eabf
Florence Bain <florence_bain@outlook.com>
2021-09-24 20:02:35 +0100

    set up chitter app

--- Gemfile
+++ Gemfile
@@ -1,11 +1,14 @@
 source 'https://rubygems.org'
 
-ruby '3.0.2'
+ruby '2.7.2'
 
 group :test do
   gem 'rspec'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
+  gem 'sinatra'
+  gem 'sinatra-contrib'
+  gem 'capybara'
 end
 
 group :development, :test do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,18 @@
 require 'simplecov'
 require 'simplecov-console'
 
+ENV['RACK_ENV'] = 'test'
+
+# Bring in the contents of the `app.rb` file. The below is equivalent to: require_relative '../app.rb'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+# Tell Capybara to talk to BookmarkManager
+Capybara.app = Chitter
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
